0b70296432bb5147c961113606f64dbee1bb4e66
Slava <jaroslav.dashkevich@gmail.com>
2016-02-03 00:08:22 +0600

    Doesn't work without bruteforce

--- README.md
+++ README.md
@@ -1,21 +1,7 @@
 Homebrew adblock for vk.com (or whatever you want to extend it for).
 
-vk.com/js/al/aes_light.js injects ad into page after adblocks fired, but if you just block it there will be a problem with navigation.
-vk works like an SPA. So far have found that disabling aes_light causes crash of "comments" link, that is handled by feed.js.
-The problem is on the line 899
-`cur.reposts = cur.options.reposts = '';`
-solution is to block both aes_light and feed, inject slightly changed feed.js back, then remove all known ads
-
-Adblock is still required, unless you know how to block selected scripts from downloading
+Just constantly remove everything
 
 ## installation
 https://developer.chrome.com/extensions/getstarted#unpacked
 
-feed.js is the modified version, original is in feed_old.js
-bg.js is a content script that clears DOM and injects feed.js
-
-popup.html and popup.js are there just in case. Right now they are empty and useless.
-
-"JavaScript error: AdsLight is not defined" will show, but didn't see it cause any problem
-
-Enjoy.
--- bg.js
+++ bg.js
@@ -1,42 +1,4 @@
-/* the problem with vk is that in common.js they check for ads presence:
-
- if (elem.id === 'left_ads' && (elem.style['visibility'] || elem.style['display']) && vk.id && (vk.id % 17 < 5)) {
-    //elem.setAttribute('style', elem.getAttribute('style').replace('visibility: visible;','visibility: visible !important;').replace('display: block;','display: block !important;'));
-    elem.style.setProperty('visibility', 'visible', 'important');
-    elem.style.setProperty('display', 'block', 'important');
-  }
-
-  * and this file will be loaded after extension script finished
-  * chrome extension can access DOM, but not window (there is a walk around, by injecting script into page, but it's cumbersome
-  * therefore bruteforce approach is used: remove, later check and remove again (aprx. loading time in my case 5-7 sec)
-  * repeat several times
-*/
-
-/* chrome extensions are a real mess - background script can't detect page change when you click a link
- * need to use content script, but that one can't access DOM
- * it became even more terrible
- */
-/*
-function searchAdParent (q) {
-	var z=q.parentElement;
-	if (z.getAttribute('class').trim() === 'feed_row') {
-		return z;
-	} else {
-		return searchAdParent(z);
-	}
-}
-* */
 if (location.host === 'vk.com') {
-	// inject modified feed.js
-	function injectScript(file, node) {
-		var th = document.getElementsByTagName(node)[0];
-		var s = document.createElement('script');
-		s.setAttribute('type', 'text/javascript');
-		s.setAttribute('src', file);
-		th.appendChild(s);
-	}
-	injectScript( chrome.extension.getURL('/feed.js'), 'body');
-	
 	function clearVkStuff (name) {
 		var tempElement = document.querySelector(name);
 		if (tempElement) {
@@ -44,25 +6,25 @@ if (location.host === 'vk.com') {
 			console.log(name + ' cleared');
 		}
 	}
+	
 	var adList = [	'#left_ads',
 					'.ads_ads_news_wrap',
 					'#feed_recommends',
-					'span[class*="promoted"]'];
+					'span[class*="promoted"]',
+					'#videocat_page'
+	];
+					
 	// remove whatever add is there and it's further sources
-	for (var i=0; i<adList.length; i++) {
-		clearVkStuff(adList[i]);
+	function burnIt () {
+		for (var i=0; i<adList.length; i++) {
+			clearVkStuff(adList[i]);
+		}
 	}
 	
-/*
+	burnIt();
 	setInterval(function() {
-		clearVkStuff();
-	}, 5000);
-*/
+		burnIt();
+	}, 500);
+
 }
-
-//getStyle.toString().replace(/if\s\(elem\.id(.|\n[^}])*}/, '').replace(/function(.)*{/, '');
-//getStyle.toString().replace(/if\s\(elem\.id(.|\n)*/, ';return ret;').replace(/function(.)*{/, '');
-//getStyle = new Function('elem', 'name', 'force', getStyle.toString().replace(/if\s\(elem\.id(.|\n)*/, ';return ret;').replace(/function(.)*{/, ''));
-
-
 
