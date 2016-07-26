9c83f7e4d0fd554689da0f58a318609f0369aebb
Yaroslav <jaroslav.dashkevich@gmail.com>
2016-07-26 23:14:32 +0700

    Big refactoring

--- ct.js
+++ ct.js
@@ -1,7 +1,7 @@
 /// <reference path="./typings/tsd.d.ts" />
 'use strict';
 
-var blockedUrl = ['vk.', 'kinopoisk.', 'ngs.'];
+var blockedUrl = ['vk.', 'kinopoisk.', 'ngs.', '2gis.'];
 // , `ngs.`];
 var handler = {};
 
@@ -14,12 +14,10 @@ function clearVkStuff (name) {
     }
 }
 
-var adList = [	'#left_ads',
-                `#ads_left`,
-                // '.ads_ads_news_wrap',
-                // '#feed_recommends',
-                // 'span[class*="promoted"]',
-                // '#videocat_page'
+var adList =
+[
+  '#left_ads',
+  `#ads_left`
 ];
 
 // remove whatever add is there and it's further sources
@@ -38,17 +36,6 @@ function burnIt () {
       }
     }
 }
-
-// function deleteAdPosts () {
-//     var fr = document.querySelectorAll('.post');
-//     for (var i=0; i < fr.length; i++) {
-//         // ad post doesn't 'response' or 'comment' options, neither it has an explanation like 'added n photos'
-//         if (!fr[i].querySelector('a[class*="repl"]') && (!fr[i].querySelector('.explain') ||
-//             !fr[i].querySelector('.explain').textContent)) {
-//             fr[i].parentElement.remove();
-//         }
-//     }
-// }
 
 function deleteVkTrash ()
 {
@@ -77,33 +64,95 @@ handler.startvk =
 /*** \vk */
 
 /*** kinopoisk */
-handler.startkinopoisk = function () {
+handler.startkinopoisk =
+  function () {
     console.log(`start kinopoisk`);
     if (!!document.querySelector('ul.menu')) {
-        var body = document.getElementsByTagName('body')[0];
-        body.style.background = '';
+      var body = document.getElementsByTagName('body')[0];
+      body.style.background = '';
     }
     var objs = document.getElementsByTagName('object');
     if (document.getElementById('top').offsetTop > 0) {
-        body.style.position = 'relative';
-        body.style.top = `-${document.getElementById('top').offsetTop}px`;
+      body.style.position = 'relative';
+      body.style.top = `-${document.getElementById('top').offsetTop}px`;
     }
     for (var k=0; k < objs.length; k++) {
-        objs[k].remove();
+      objs[k].remove();
     }
 }
 /*** \kinopoisk */
 
 /*** ngs */
-handler.startngs = function () {
+handler.startngs =
+  function () {
     findAndRemoveTrash( document.querySelector('.edition-phone-footer') );
 
-    var popups = document.querySelectorAll(`div[class*="popup"]`);
-    for (var i=0; i < popups.length; i++) {
-        popups[i].remove();
-    }
-}
+    removeArrSelectors(
+      document.querySelectorAll(`div[class*="popup"]`)
+    );
+
+    removeArrSelectors(
+      document.querySelectorAll(`div[class*="banner"]`)
+    );
+
+    removeArrSelectors(
+      document.querySelectorAll(`div[id*="div-gpt-ad"]`)
+    );
+
+    var observer = new MutationObserver(
+      mutations =>
+      {
+        console.log(mutations);
+        for (var i = 0; i < mutations.length; i++)
+        {
+          mutations[i].target.style.overflow = '';
+        }
+      }
+    );
+
+    var targetBody = document.getElementsByTagName('body')[0];
+    observer.observe(targetBody, { attributes : true, attributeFilter : ['style'] });
+    var targetHtml = document.getElementsByTagName('html')[0];
+    observer.observe(targetHtml, { attributes : true, attributeFilter : ['style'] });
+
+  };
 /*** /ngs */
+
+/** 2gis */
+handler.start2gis =
+  () =>
+  { // it loads later -> look for it for 5 sec
+    var clear1 = false, clear2 = false, counter = 0;
+
+    const search =
+      setInterval(
+        () =>
+        {
+          var target = document.querySelector('.dashboard__promo');
+          if ( target.textContent.match('Adblock') )
+          {
+            target.remove();
+            clear1 = true;
+          }
+          target = document.querySelector('.promoTooltip');
+          if ( target )
+          {
+            target.remove();
+            clear2 = true;
+          }
+
+          counter++;
+          if ( (clear1 && clear2) || counter > 25)
+          {
+            clearInterval(search);
+          }
+        },
+        200
+      );
+  };
+
+
+/** /2gis */
 
 /** processor
  *
@@ -115,6 +164,53 @@ for (var i=0; i < blockedUrl.length; i++) {
         break;
     }
 }
+
+/** yandex direct */
+setTimeout(
+  () =>
+  {
+    eliminateYandex(
+      document.querySelectorAll('[id*="direct"]')
+    );
+
+    eliminateYandex(
+      document.querySelectorAll('[class*="direct"]')
+    );
+
+    removeArrSelectors(
+      document.querySelectorAll('.adv.adv_pos_top')
+    );
+  },
+  1500
+);
+
+var elYaCount = 0;
+function eliminateYandex (arr)
+{
+  for (var i = 0; i < arr.length; i++)
+  {
+    console.log('eliminate yandex: ' + (++elYaCount) );
+
+    var cl = arr[i].getAttribute('class');
+    var id = arr[i].getAttribute('id');
+    if (
+      cl && cl.match('yandex') ||
+      id && id .match('yandex')
+    )
+    {
+      arr[i].remove();
+    }
+  }
+}
+
+function removeArrSelectors (arr)
+{
+  for (var i = 0; i < arr.length; i++)
+  {
+    arr[i].remove();
+  }
+}
+
 
 function findAndRemoveTrash (trash)
 {
