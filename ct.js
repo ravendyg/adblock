fabc107e189e849446ee2a41a14aca0b728bec6a
Yaroslav <jaroslav.dashkevich@gmail.com>
2017-12-03 00:09:02 +0700

    Change structure, fix yandex.ru/pogoda, disable yandex - not sure whether is's up to date.

--- ct.js
+++ ct.js
@@ -1,9 +1,160 @@
 /// <reference path="./typings/tsd.d.ts" />
 'use strict';
 
-var blockedUrl = ['vk.', 'kinopoisk.', 'ngs.', '2gis.', 'yandex.'];
-// , `ngs.`];
-var handler = {};
+const blocker = {
+  vk: {
+    reg: /vk\./,
+    cleaner() {
+      console.log(`start vk`);
+
+      burnIt();
+      deleteVkTrash();
+
+      setInterval(
+        () => {
+          burnIt();
+          deleteVkTrash();
+        }, 200
+      );
+    },
+  },
+
+  kinopoisk: {
+    reg: /kinopoisk\./,
+    cleaner() {
+      console.log(`start kinopoisk`);
+      document.querySelector('html').style.background = 'none';
+      if (!!document.querySelector('ul.menu')) {
+        var body = document.getElementsByTagName('body')[0];
+        body.style.background = '';
+      }
+      var objs = document.getElementsByTagName('object');
+      // if (document.getElementById('top').offsetTop > 0) {
+      //   body.style.position = 'relative';
+      //   body.style.top = `-${document.getElementById('top').offsetTop}px`;
+      // }
+      for (var k=0; k < objs.length; k++) {
+        objs[k].remove();
+      }
+
+      var bf = document.getElementById('brandingFlash');
+      if ( bf ) { bf.remove(); }
+
+      // shift menu to the top
+      var form = document.getElementById('top_form');
+      var menu = document.querySelector('ul.menu');
+      var block = document.querySelector('div.png_block');
+      var top = document.getElementById('top');
+      if ( form && menu && block )
+      {
+        form.style.top  = '10px';
+        menu.style.top  = '10px';
+        block.style.top = '10px';
+        top.style.height = (block.offsetHeight + 3) + 'px';
+      }
+      var someBanner = document.querySelector('a[href*="https://awaps"]');
+      someBanner && someBanner.parentElement.remove();
+    }
+  },
+
+  ngs: {
+    reg: /ngs\./,
+    cleaner() {
+      findAndRemoveTrash( document.querySelector('.edition-phone-footer') );
+
+          removeArrSelectors(
+            document.querySelectorAll(`div[class*="popup"]`)
+          );
+
+          removeArrSelectors(
+            document.querySelectorAll(`div[class*="banner"]`)
+          );
+
+          removeArrSelectors(
+            document.querySelectorAll(`div[id*="div-gpt-ad"]`)
+          );
+
+          removeArrSelectors(
+            document.querySelectorAll('[src*="reklama"]')
+          );
+
+          removeArrSelectors(
+            document.querySelectorAll('[href*="reklama"]')
+          );
+
+          var observer = new MutationObserver(
+            mutations =>
+            {
+              console.log(mutations);
+              for (var i = 0; i < mutations.length; i++)
+              {
+                mutations[i].target.style.overflow = '';
+              }
+            }
+          );
+
+          var targetBody = document.getElementsByTagName('body')[0];
+          observer.observe(targetBody, { attributes : true, attributeFilter : ['style'] });
+          var targetHtml = document.getElementsByTagName('html')[0];
+          observer.observe(targetHtml, { attributes : true, attributeFilter : ['style'] });
+      }
+  },
+
+  gis2: {
+    reg: /2gis\./,
+    cleaner() {
+      let
+        clear1 = false,
+        clear2 = false,
+        counter = 0
+        ;
+      const search =
+        setInterval(
+          () =>
+          {
+            var target = document.querySelector('.dashboard__promo');
+            if ( target.textContent.match('Adblock') )
+            {
+              target.remove();
+              clear1 = true;
+            }
+            target = document.querySelector('.promoTooltip');
+            if ( target )
+            {
+              target.remove();
+              clear2 = true;
+            }
+
+            counter++;
+            if ( (clear1 && clear2) || counter > 25)
+            {
+              clearInterval(search);
+            }
+          },
+          200
+        );
+    }
+  },
+
+  pogoda: {
+    reg: /yandex\.ru\/pogoda/,
+    cleaner() {
+      const content = document.querySelector('.forecast-details');
+      if (!content) return;
+
+      const forecastReg = /^forecast/;
+      const rows = [...content.children];
+      rows.forEach(row => {
+        const classes = (row.getAttribute('class') || '').split(/\s/);
+        classes.forEach(klass => {
+          if (!forecastReg.test(klass)) {
+            row.remove();
+          }
+        })
+      });
+    }
+  },
+}
 
 /*** vk */
 function clearVkStuff (name) {
@@ -48,149 +199,11 @@ function burnIt () {
     }
 }
 
-function deleteVkTrash ()
-{
+function deleteVkTrash() {
   findAndRemoveTrash( document.getElementById('video_content_catalog') );
   findAndRemoveTrash( document.getElementById('videocat_other_blocks') );
   findAndRemoveTrash( document.getElementById('video_content_catalog') );
 }
-
-handler.startvk =
- function ()
- {
-   console.log(`start vk`);
-
-   burnIt();
-   deleteVkTrash();
-
-   setInterval(
-     () =>
-     {
-       burnIt();
-       deleteVkTrash();
-     }
-     , 200
-   );
- }
-/*** \vk */
-
-/*** kinopoisk */
-handler.startkinopoisk =
-  function () {
-    console.log(`start kinopoisk`);
-    document.querySelector('html').style.background = 'none';
-    if (!!document.querySelector('ul.menu')) {
-      var body = document.getElementsByTagName('body')[0];
-      body.style.background = '';
-    }
-    var objs = document.getElementsByTagName('object');
-    // if (document.getElementById('top').offsetTop > 0) {
-    //   body.style.position = 'relative';
-    //   body.style.top = `-${document.getElementById('top').offsetTop}px`;
-    // }
-    for (var k=0; k < objs.length; k++) {
-      objs[k].remove();
-    }
-
-    var bf = document.getElementById('brandingFlash');
-    if ( bf ) { bf.remove(); }
-
-    // shift menu to the top
-    var form = document.getElementById('top_form');
-    var menu = document.querySelector('ul.menu');
-    var block = document.querySelector('div.png_block');
-    var top = document.getElementById('top');
-    if ( form && menu && block )
-    {
-      form.style.top  = '10px';
-      menu.style.top  = '10px';
-      block.style.top = '10px';
-      top.style.height = (block.offsetHeight + 3) + 'px';
-    }
-    var someBanner = document.querySelector('a[href*="https://awaps"]');
-    someBanner && someBanner.parentElement.remove();
-}
-/*** \kinopoisk */
-
-/*** ngs */
-handler.startngs =
-  function () {
-    findAndRemoveTrash( document.querySelector('.edition-phone-footer') );
-
-    removeArrSelectors(
-      document.querySelectorAll(`div[class*="popup"]`)
-    );
-
-    removeArrSelectors(
-      document.querySelectorAll(`div[class*="banner"]`)
-    );
-
-    removeArrSelectors(
-      document.querySelectorAll(`div[id*="div-gpt-ad"]`)
-    );
-
-    removeArrSelectors(
-      document.querySelectorAll('[src*="reklama"]')
-    );
-
-    removeArrSelectors(
-      document.querySelectorAll('[href*="reklama"]')
-    );
-
-    var observer = new MutationObserver(
-      mutations =>
-      {
-        console.log(mutations);
-        for (var i = 0; i < mutations.length; i++)
-        {
-          mutations[i].target.style.overflow = '';
-        }
-      }
-    );
-
-    var targetBody = document.getElementsByTagName('body')[0];
-    observer.observe(targetBody, { attributes : true, attributeFilter : ['style'] });
-    var targetHtml = document.getElementsByTagName('html')[0];
-    observer.observe(targetHtml, { attributes : true, attributeFilter : ['style'] });
-
-  };
-/*** /ngs */
-
-/** 2gis */
-handler.start2gis =
-  () =>
-  { // it loads later -> look for it for 5 sec
-    var clear1 = false, clear2 = false, counter = 0;
-
-    const search =
-      setInterval(
-        () =>
-        {
-          var target = document.querySelector('.dashboard__promo');
-          if ( target.textContent.match('Adblock') )
-          {
-            target.remove();
-            clear1 = true;
-          }
-          target = document.querySelector('.promoTooltip');
-          if ( target )
-          {
-            target.remove();
-            clear2 = true;
-          }
-
-          counter++;
-          if ( (clear1 && clear2) || counter > 25)
-          {
-            clearInterval(search);
-          }
-        },
-        200
-      );
-  };
-
-
-/** /2gis */
 
 /*
 handler.startyandex = () => {
@@ -206,11 +219,12 @@ handler.startyandex = () => {
  *
  * start up remove tasks
  */
-for (var i=0; i < blockedUrl.length; i++) {
-    if (location.href.match(blockedUrl[i])) {
-        handler[`start${blockedUrl[i].replace(/\./g,'')}`]();
-        break;
-    }
+for (let key of Object.keys(blocker)) {
+  debugger
+  if (blocker[key].reg.test(location.href)) {
+    blocker[key].cleaner();
+    break;
+  }
 }
 
 /** yandex direct */
@@ -218,42 +232,42 @@ var elYaCount = 0;
 clearAllNonRepeptitiveStuff();
 setTimeout(clearAllNonRepeptitiveStuff, 1500);
 
-function eliminateYandexAll ()
-{
-  var xSelector = document.evaluate("//div[text()=\"Яндекс.Директ\"]", document.documentElement, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
-  for (var i = 0; i < xSelector.snapshotLength; i++)
-  {
-    eliminateParent(xSelector.snapshotItem(i));
-  }
-  eliminateYandex(
-    document.querySelectorAll('[id*="direct"]')
-  );
+// function eliminateYandexAll ()
+// {
+//   var xSelector = document.evaluate("//div[text()=\"Яндекс.Директ\"]", document.documentElement, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+//   for (var i = 0; i < xSelector.snapshotLength; i++)
+//   {
+//     eliminateParent(xSelector.snapshotItem(i));
+//   }
+//   eliminateYandex(
+//     document.querySelectorAll('[id*="direct"]')
+//   );
 
-  eliminateYandex(
-    document.querySelectorAll('[class*="direct"]')
-  );
+//   eliminateYandex(
+//     document.querySelectorAll('[class*="direct"]')
+//   );
 
-  removeArrSelectors(
-    document.querySelectorAll('.adv.adv_pos_top')
-  );
-}
-function eliminateYandex (arr)
-{
-  for (var i = 0; i < arr.length; i++)
-  {
-    console.log('eliminate yandex: ' + (++elYaCount) );
+//   removeArrSelectors(
+//     document.querySelectorAll('.adv.adv_pos_top')
+//   );
+// }
+// function eliminateYandex (arr)
+// {
+//   for (var i = 0; i < arr.length; i++)
+//   {
+//     console.log('eliminate yandex: ' + (++elYaCount) );
 
-    var cl = arr[i].getAttribute('class');
-    var id = arr[i].getAttribute('id');
-    if (
-      cl && cl.match('yandex') ||
-      id && id .match('yandex')
-    )
-    {
-      arr[i].remove();
-    }
-  }
-}
+//     var cl = arr[i].getAttribute('class');
+//     var id = arr[i].getAttribute('id');
+//     if (
+//       cl && cl.match('yandex') ||
+//       id && id .match('yandex')
+//     )
+//     {
+//       arr[i].remove();
+//     }
+//   }
+// }
 
 function eliminateParent(target)
 {
@@ -273,9 +287,8 @@ function eliminateVelumAll ()
   }
 }
 
-function clearAllNonRepeptitiveStuff ()
-{
-  eliminateYandexAll();
+function clearAllNonRepeptitiveStuff () {
+  // eliminateYandexAll();
   eliminateVelumAll();
 }
 
